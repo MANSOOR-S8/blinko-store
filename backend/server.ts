3715ba2d5f8a8912a4bd106f3d32e330df12ad42
cpt.mansoor <92313207+MANSOOR-S8@users.noreply.github.com>
2026-04-03 21:27:28 +0500

    change ts config  sitting

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,4 @@
-import app from "./app";
+import app from "./app.ts";
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port no ${PORT}`);
--- src/components/dashboard/StatsCard.tsx
+++ src/components/dashboard/StatsCard.tsx
@@ -1,1 +1,1 @@
-﻿export default function Placeholder() { return <div>'src/components/dashboard/StatsCard.tsx'</div>; }
+﻿
